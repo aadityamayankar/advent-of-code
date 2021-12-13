90223450b86299d1dff67dcaf3d9c30148ba91bb
mayankar <aadityamayankar@gmail.com>
2021-12-13 16:11:04 +0530

    Day 1

---
+++ Day1/1A.py
@@ -0,0 +1,12 @@
+def solve(nums):
+    cnt = 0
+    for i in range(1,len(nums),1):
+        if nums[i] > nums[i-1]:
+            cnt += 1
+    print(cnt)
+
+if __name__ == "__main__":
+    f = open("input.txt", mode='r')
+    s = f.read()
+    solve(list(map(int,s.split('\n'))))
+    f.close()
